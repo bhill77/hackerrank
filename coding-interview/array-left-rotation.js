64c3679d55c4c659032d3dda8316479d4b840d8b
Abdul Latif Syafi'i <a.l.syafii@gmail.com>
2017-07-08 11:53:54 +0700

    July 8, 2017

---
+++ coding-interview/array-left-rotation.js
@@ -0,0 +1,38 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var n_temp = readLine().split(' ');
+    var n = parseInt(n_temp[0]);
+    var k = parseInt(n_temp[1]);
+    a = readLine().split(' ');
+    
+    a.rotate(k);
+    
+    console.log(a.join(' '));
+
+}
+
+Array.prototype.rotate = function( n ) {
+  this.unshift.apply( this, this.splice( n, this.length ) )
+  return this;
+}
